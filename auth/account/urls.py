b63fbbb4a436e6cdeae2122e896313f474f61acb
Aditya Thute <aadityathute@gmail.com>
2025-01-26 19:20:28 +0530

    Added Logout from Apps But not from main server

--- auth/account/urls.py
+++ auth/account/urls.py
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('o/logout/', views.auth_logout_view, name='auth_logout'),
 ]
--- auth/account/views.py
+++ auth/account/views.py
@@ -4,6 +4,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from oidc_provider.models import Token
+from django.views.decorators.csrf import csrf_exempt
 
 # Define the login view function
 def login_view(request):
@@ -32,5 +34,49 @@ def login_view(request):
  # 🔑 Only authenticated users can access this view
 @login_required
 def logout_view(request):
+    next_url = request.GET.get('next', 'index')  # Default to `index` if not provided
+
+    try:
+        # Retrieve and delete all tokens associated with the user to invalidate them
+        tokens = Token.objects.filter(user=request.user)
+        if tokens.exists():
+            tokens.delete()
+            print(f"Deleted tokens for user: {request.user}")
+        else:
+            print("No tokens found for user.")
+    except Exception as e:
+        print(f"Error deleting tokens: {e}")
+
+    # Log out the user locally (clear session)
     logout(request)
-    return redirect('index')
+
+    return redirect(next_url)
+
+@csrf_exempt
+def auth_logout_view(request):
+    next_url = request.GET.get('next', 'index')  # Default to `index` if not provided
+    user_id = request.POST.get('user_id')  # Get the `user_id` (user ID) from the POST data
+    print('request', request)
+    print('user____id', user_id)
+
+    try:
+        # Retrieve and delete all tokens associated with the user to invalidate them
+        tokens = Token.objects.filter(user=user_id)
+        if tokens.exists():
+            tokens.delete()
+            print(f"Deleted tokens for user: {request.user}")
+        else:
+            print("No tokens found for user.")
+    except Exception as e:
+        print(f"Error deleting tokens: {e}")
+
+    # Log out the user locally (clear session)
+    logout(request)
+    print(f"User logged out: {request.user}")
+
+    # Delete the current session
+    session_id = request.session.session_key
+    print(session_id)
+
+    request.session.flush()  # Clears the session data
+    return redirect(next_url)
